25e9a1e36d17f8dd9b64ba7a1c345d741e1693e1
dropsy <dropsy422@gmail.com>
2023-05-13 15:16:09 +0600

    Add project files.

---
+++ ADODesigner/Models/Project.cs
@@ -0,0 +1,18 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+#nullable disable
+
+namespace ADODesigner.Models
+{
+    public partial class Project : ObservableObject
+    {
+        [ObservableProperty]
+        private string path;
+        [ObservableProperty]
+        private string name;
+    }
+}
---
+++ ADODesigner/ViewModels/HomeVM.cs
@@ -0,0 +1,32 @@
+﻿using ADODesigner.Models;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+#nullable disable
+
+namespace ADODesigner.ViewModels
+{
+    public partial class HomeVM : ObservableObject
+    {
+        [ObservableProperty]
+        private Project selectedProject;
+        [ObservableProperty]
+        private ObservableCollection<Project> projects;
+
+        [RelayCommand]
+        private void CreateProject()
+        {
+
+        }
+        [RelayCommand]
+        private void OpenProject()
+        {
+
+        }
+    }
+}
