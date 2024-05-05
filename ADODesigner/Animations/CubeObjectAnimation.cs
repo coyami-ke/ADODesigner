de8b517c22532d6695747f116e994d69ce175e35
coyami-ke <dropsy422@gmail.com>
2024-05-05 10:58:06 +0500

    Added Cube Object Animation

--- ADODesigner/Animations/CubeObjectAnimation.cs
+++ ADODesigner/Animations/CubeObjectAnimation.cs
@@ -1,4 +1,5 @@
 ﻿using ADODesigner.Models;
+using ADODesigner.Reflection;
 using System;
 using System.Collections.Generic;
 using System.Drawing;
@@ -9,110 +10,117 @@ using System.Threading.Tasks;
 
 namespace ADODesigner.Animations
 {
-    public class CubeObjectAnimationArgs
+    public class CubeObjectAnimation : ICloneable
     {
-        public const float SIZE_PIXEL = 0.0066666666667f;
-        public Vector2 Scale { get; set; } = new(150);
+        public const float SIZE_PIXEL = ADOFAIConst.SIZE_PIXEL_IN_TILES;
+        [UsageWindowProperties(AddToWindowProperties = false)]
+        public int Floor { get; set; } = 1;
+        public string ImageRectangle { get; set; } = "rectangle.png";
+        public Vector2 Position { get; set; } = new();
+        public float SettingSides { get; set; } = 3;
+        public float Scale { get; set; } = 150;
         public float Parallax { get; set; } = 50;
-        public string ImageRectange { get; set; } = "rectange.png";
+        public Vector2 ParallaxOffsetSides { get; set; } = new();
+        public float Opacity { get; set; } = 100;
+        public int Depth { get; set; } = 50;
+        public float ParallaxMultiplier { get; set; } = 4;
         public string ColorLeftSide { get; set; } = "ffffffff";
         public string ColorRightSide { get; set; } = "ffffffff";
         public string ColorFrontSide { get; set; } = "ffffffff";
         public string ColorTopSide { get; set; } = "ffffffff";
         public string Tag { get; set; } = "ADODesigner_Cube";
-        public Vector2 Position { get; set; } = new();
-    }
-    public class CubeObjectAnimation : BaseAnimation<CubeObjectAnimationArgs>
-    {
 
-        public CubeObjectAnimation(CubeObjectAnimationArgs args) : base(args)
+        public Decoration[] Animate()
         {
-        }
-        public Vector2 GetParallax(int i)
-        {
-            return new(Args.Parallax + (i * ADOFAIConst.SIZE_PIXEL_IN_TILES * 2f));
-        }
-        public override (KeyFrame[], Decoration[]) CreateAnimation()
-        {
+            List<Decoration> list = new();
+
             Decoration frontSide = new();
-            frontSide.Scale = Args.Scale;
-            frontSide.Color = Args.ColorFrontSide;
-            frontSide.Parallax = new(Args.Parallax);
-            frontSide.Image = Args.ImageRectange;
-            frontSide.Position = Args.Position;
-            frontSide.Depth = Convert.ToInt32(Args.Parallax);
+            frontSide.Scale = new(Scale);
+            frontSide.Floor = Floor;
+            frontSide.Position = Position;
+            frontSide.Color = ColorFrontSide;
+            frontSide.Depth = Depth;
+            frontSide.Tag = Tag;
+            frontSide.Parallax = new(Parallax);
+            frontSide.Image = ImageRectangle;
 
-            List<Decoration> result = new();
-            List<Decoration> rightSide = new();
-            List<Decoration> leftSide = new();
-            List<Decoration> topSide = new();
-            List<Decoration> bottomSide = new();
+            float tPositionSides = (1 - (Parallax / 100)) * (SettingSides * (Scale / 150)); // 25% = 1.25; 50% = 3; 75% = 12; 90 = 75; 80 = ? // Multiplayer = 5 
+            float tOffset = SIZE_PIXEL * (tPositionSides / 2);
 
-            // Top Side
-            for (int i = 0; i < Args.Scale.Y; i++)
+            Vector2 parallaxOffsetSides = SIZE_PIXEL * ParallaxOffsetSides;
+
+            // top side
+            for (int i = 0; i < Scale; i++)
             {
-                Decoration deco = new();
-                deco.Image = Args.ImageRectange;
-                deco.ParallaxOffset = new(0, 0.75f);
-                deco.Color = Args.ColorTopSide;
+                Decoration topSide = CreateNewDecoration();
+                topSide.Scale = new(Scale - (i / 2), 1);
+                topSide.Position = Position;
+                topSide.Color = ColorTopSide;
+                topSide.Parallax = new(Parallax + SIZE_PIXEL * i * ParallaxMultiplier);
+                topSide.ParallaxOffset = new Vector2(0, Scale / 150 - (0.25f * (Scale / 150))) + parallaxOffsetSides * -i;
+                topSide.Depth += 3;
 
-                deco.Scale = new(Args.Scale.X - (i / 2), 1);
-                deco.Parallax = GetParallax(i);
-                deco.Depth = (int)Args.Parallax;
-                deco.Position = new(0, 0);
-
-                deco.Tag = $"{Args.Tag} {Args.Tag}_TopSide";
-
-                topSide.Add(deco);
-            }
-            // Right Side
-            for (int i = 0; i < Args.Scale.X; i++)
-            {
-                Decoration deco = new();
-                deco.Image = Args.ImageRectange;
-                deco.ParallaxOffset = new(0.75f, 0);
-                deco.Color = Args.ColorRightSide;
-
-                deco.Scale = new(1, Args.Scale.Y - (i / 2));
-                deco.Parallax = GetParallax(i);
-                deco.Depth = (int)Args.Parallax + 1;
-                deco.Position = new(-(i * ADOFAIConst.SIZE_PIXEL_IN_TILES * 0.75f), (i * ADOFAIConst.SIZE_PIXEL_IN_TILES / 1.25f));
-
-                deco.Tag = $"{Args.Tag} {Args.Tag}_RightSide";
-
-                rightSide.Add(deco);
-            }
-            // Left Side
-            for (int i = 0; i < Args.Scale.X; i++)
-            {
-                Decoration deco = new();
-                deco.Image = Args.ImageRectange;
-                deco.ParallaxOffset = new(-0.75f, 0);
-                deco.Color = Args.ColorLeftSide;
-
-                deco.Scale = new(1, Args.Scale.Y - (i / 2)); ;
-                deco.Parallax = GetParallax(i); ;
-                deco.Depth = (int)Args.Parallax + 2;
-                deco.Position = new(i * ADOFAIConst.SIZE_PIXEL_IN_TILES * 0.75f, (i * ADOFAIConst.SIZE_PIXEL_IN_TILES / 1.25f));
-
-                deco.Tag = $"{Args.Tag} {Args.Tag}_LeftSide";
-                leftSide.Add(deco);
-                
+                list.Add(topSide);
             }
 
-            result.AddRange(topSide.ToArray());
-            result.AddRange(rightSide.ToArray());
-            result.AddRange(leftSide.ToArray());
-            result.AddRange(bottomSide.ToArray());
-            result.Add(frontSide);
-
-            for (int i = 0; i < result.Count; i++)
+            // right side
+            for (int i = 0; i < Scale; i++)
             {
-                Console.WriteLine(result[i].ToString());
-                result[i].Position += Args.Position;             
+                Decoration rightSide = CreateNewDecoration();
+                rightSide.Scale = new(1, Scale - (i / 2));
+                rightSide.Color = ColorRightSide;
+
+                Vector2 newPosition = new(tPositionSides - i * tOffset, i * tOffset);
+
+                rightSide.Position = newPosition + Position;
+                rightSide.Parallax = new(Parallax + SIZE_PIXEL * i * ParallaxMultiplier);
+
+                rightSide.ParallaxOffset = parallaxOffsetSides * -i;
+
+                rightSide.Depth += 2;
+                list.Add(rightSide);
+            }
+            // left side
+            for (int i = 0; i < Scale; i++)
+            {
+                Decoration leftSide = CreateNewDecoration();
+                leftSide.Scale = new(1, Scale - (i / 2));
+                leftSide.Color = ColorLeftSide;
+
+                Vector2 newPosition = new(-(tPositionSides - i * tOffset), i * tOffset);
+
+                leftSide.Position = newPosition + Position;
+                leftSide.Parallax = new(Parallax + SIZE_PIXEL * i * ParallaxMultiplier);
+
+                leftSide.ParallaxOffset = parallaxOffsetSides * -i;
+
+                leftSide.Depth += 2;
+                list.Add(leftSide);
             }
 
-            return (Array.Empty<KeyFrame>(), result.ToArray());
+            list.Add(frontSide);
+
+            foreach (var deco in list)
+            {
+                deco.Position += Position;
+            }
+            return list.ToArray();
+        }
+        public object Clone()
+        {
+            return (CubeObjectAnimation)this.MemberwiseClone();
+        }
+        public Decoration CreateNewDecoration()
+        {
+            Decoration decoration = new()
+            {
+                Floor = Floor,
+                Depth = Depth,
+                Image = ImageRectangle,
+                Tag = Tag,
+                Opacity = Opacity,
+            };
+            return decoration;
         }
     }
 }
